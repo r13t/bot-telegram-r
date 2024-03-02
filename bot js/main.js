2f20d199d6e9e7c28513ff47300377da6ace55ec
Abdulrahman <137515631+r13t@users.noreply.github.com>
2024-03-03 00:43:41 +0300

    main.js

--- bot js/main.js
+++ bot js/main.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '6671244143:AAHflfX9Y_eLWJi1qXiVKCkqjOsydfdUS_U'; // استبدله بتوكن البوت الخاص بك
+const token = '6671244143:AAHflfX9Y_eLWJi1qXiVKCkqjOsydfdUS_U'; 
 const bot = new TelegramBot(token, { polling: true });
 
 let accounts = JSON.parse(fs.readFileSync('accounts.json', 'utf8'));
 
-// رسالة الترحيب والزرين
+
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const userName = msg.from.first_name; // اسم المستخدم
+    const userName = msg.from.first_name; 
 
     const options = {
         reply_markup: {
@@ -35,7 +35,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(chatId, welcomeMessage, options);
 });
 
-// ردود الاستجابة على الأزرار
+
 bot.on('callback_query', (callbackQuery) => {
     const chatId = callbackQuery.message.chat.id;
     const data = callbackQuery.data;
@@ -52,7 +52,7 @@ bot.on('callback_query', (callbackQuery) => {
     }
 });
 
-// رد على الكود المدخل وإظهار المعلومات
+
 bot.onText(/^(?!\/start)(?!\/buy)(.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const userInput = match[1];
@@ -65,7 +65,7 @@ bot.onText(/^(?!\/start)(?!\/buy)(.+)/, (msg, match) => {
 
         bot.sendMessage(chatId, purchaseMessage);
 
-        // حذف الحساب المباع من المصفوفة
+     
         accounts = accounts.filter(acc => acc.asss[0].username !== userInput);
         fs.writeFileSync('accounts.json', JSON.stringify(accounts, null, 4));
     } else {
